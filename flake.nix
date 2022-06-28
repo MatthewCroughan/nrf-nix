0b3c9e3010198476f2dc232df97c43dc75dfdb11
matthewcroughan <matt@croughan.sh>
2022-06-28 01:19:31 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs2111.url = "github:nixos/nixpkgs/nixos-21.11";
     stargate-nixpkgs.url = "github:stargate01/nixpkgs/nrf-command-line-tools";
   };
-  outputs = { self, nixpkgs, stargate-nixpkgs }:
+  outputs = { self, nixpkgs2111, nixpkgs, stargate-nixpkgs }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+      pkgs2111 = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
       stargate-pkgs = import stargate-nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     in
     {
@@ -16,7 +18,7 @@
           pkgs.python310Packages.west
 
           # Minimal
-          pkgs.nrfutil
+          pkgs2111.nrfutil
           (stargate-pkgs.segger-jlink.override { acceptLicense = true; })
           stargate-pkgs.nrf-command-line-tools
         ];
